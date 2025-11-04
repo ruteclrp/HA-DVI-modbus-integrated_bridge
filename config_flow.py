a6203918770622196d148bd7975b9012ac15e29b
Lars Robert Pedersen <115367766+ruteclrp@users.noreply.github.com>
2025-11-04 15:34:40 +0100

    Update config_flow.py

--- config_flow.py
+++ config_flow.py
@@ -1,1 +1,23 @@
+from homeassistant import config_entries
+import voluptuous as vol
+from .const import DOMAIN
+
+class DviBridgeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        errors = {}
+
+        if user_input is not None:
+            return self.async_create_entry(title="DVI Bridge", data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required("port", default="/dev/serial/by-id/..."): str,
+                vol.Required("slave_id", default=16): int,
+                vol.Optional("poll_interval", default=30): int
+            }),
+            errors=errors
+        )
 
