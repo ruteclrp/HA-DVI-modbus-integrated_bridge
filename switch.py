f34839dc8dc337c37d28e3311ac1043b5d7e0c18
Lars Robert Pedersen <115367766+ruteclrp@users.noreply.github.com>
2025-11-04 15:24:03 +0100

    Update switch.py

--- switch.py
+++ switch.py
@@ -1,38 +1,43 @@
 from homeassistant.components.switch import SwitchEntity
+from .modbus_bridge import DviModbusBridge
 
-async def async_setup_entry(hass, entry, async_add_entities):
-    instrument = hass.data["dvi_bridge"].get("instrument")
-    switches = [
-        DviSwitch("Heating element", 3, instrument),
-        DviSwitch("Circ. pump warm side", 4, instrument),
-    ]
+# Map your writable Modbus registers to switch names
+SWITCHES = {
+    0x10A: "VV State",
+    0x101: "CV State",
+    0x102: "CV Curve",
+    0x10B: "VV Setpoint",
+    0x10F: "TV State"
+}
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    bridge = DviModbusBridge("/dev/serial/by-id/usb-STMicroelectronics_STM32_Virtual_COM_Port_48D874673036-if00", 0x10)
+    switches = [DviSwitch(name, reg, bridge) for reg, name in SWITCHES.items()]
     async_add_entities(switches)
 
 class DviSwitch(SwitchEntity):
-    def __init__(self, name, coil, instrument):
-        self._attr_name = f"DVI {name}"
-        self._coil = coil
-        self._instrument = instrument
-        self._state = False
+    def __init__(self, name, register, bridge):
+        self._name = name
+        self._register = register
+        self._bridge = bridge
+        self._state = None
+
+    @property
+    def name(self):
+        return self._name
 
     @property
     def is_on(self):
         return self._state
 
     async def async_turn_on(self, **kwargs):
-        await self.hass.async_add_executor_job(
-            self._instrument.write_bit, self._coil, 1
-        )
+        self._bridge.write_register(self._register, 1)
         self._state = True
 
     async def async_turn_off(self, **kwargs):
-        await self.hass.async_add_executor_job(
-            self._instrument.write_bit, self._coil, 0
-        )
+        self._bridge.write_register(self._register, 0)
         self._state = False
 
     async def async_update(self):
-        val = await self.hass.async_add_executor_job(
-            self._instrument.read_bit, self._coil
-        )
+        val = self._bridge.read_via_fc06(self._register)
         self._state = bool(val)
